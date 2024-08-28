613aca8c97e90cd877ead38f900c792fa594d5c5
Eduardos13 <edik.kozelsky@gmail.com>
2024-08-28 18:46:07 -0400

    +

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Modal from "react-modal";
-import s from "./ImageModal.module.css";
-import { RxCross2 } from "react-icons/rx";
+import React from 'react';
+import Modal from 'react-modal';
+import s from './ImageModal.module.css';
+import { RxCross2 } from 'react-icons/rx';
 
 const ImageModal = ({ image, onClose }) => {
   return (
